f1b0e50ce7ee90d6400b5612bfa3fb3c057f1c53
maallard <mar.allard@gmail.com>
2017-04-18 18:24:02 +0200

    Ajout de la vérification d ela solution

--- app/js/controllers/games/boucheTrouController.js
+++ app/js/controllers/games/boucheTrouController.js
@@ -1,13 +1,28 @@
 angular.module('app')
     .controller('BoucheTrouController', function($scope) {
-        $scope.holes = [];
-        $scope.words = [
-            'blesser',
+        /*
+        * La solution (les mots sont dans le bonne ordre)
+        */
+        var WORDS = [
+            'débrancher',
             'électrique',
             'nettoyer',
-            'débrancher'
+            'blesser'
         ];
-        $scope.onDrop = function(e) {
-            console.log("DROPPED:", e);
+        Object.freeze(WORDS); // rend constant WORDS
+
+
+        $scope.words = WORDS.slice(0); // copie l'array
+        $scope.holes = WORDS.map( function() { return ""; }); // pour que words et holes soient bien de la même longueure
+
+        $scope.onDrop = function() {
+            console.log(checkSolution($scope.holes));
+            console.log('WORDS', WORDS, 'solution', $scope.holes);
         };
+
+        function checkSolution(proposedSolution) {
+            return proposedSolution.length === WORDS.length && proposedSolution.every( function(word, index) {
+                return word === WORDS[index];
+            });
+        }
     });
