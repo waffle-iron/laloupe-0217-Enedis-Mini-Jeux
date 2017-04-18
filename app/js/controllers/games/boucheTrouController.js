35ace0930d0aba3b104a08af22ce15a504e3ee60
maallard <mar.allard@gmail.com>
2017-04-18 17:16:38 +0200

    Ajout des fonctionnalités de base du texte à trous : déplacage des mots dans le texte

--- app/js/controllers/games/boucheTrouController.js
+++ app/js/controllers/games/boucheTrouController.js
@@ -1,4 +1,13 @@
 angular.module('app')
     .controller('BoucheTrouController', function($scope) {
-
+        $scope.holes = [];
+        $scope.words = [
+            'blesser',
+            'électrique',
+            'nettoyer',
+            'débrancher'
+        ];
+        $scope.onDrop = function(e) {
+            console.log("DROPPED:", e);
+        };
     });
