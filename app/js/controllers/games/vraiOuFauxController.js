ed16a67e452323b3c5b547f88572deb079aa3151
Cleclemi28 <c.trofleau@gmail.com>
2017-04-19 11:06:29 +0200

    changement de design avec gros bouton Vrai ou Faux

--- app/js/controllers/games/vraiOuFauxController.js
+++ app/js/controllers/games/vraiOuFauxController.js
@@ -1,5 +1,10 @@
 //Controller du jeux Vrai ou Faux du livret
 angular.module('app')
     .controller('VraiOuFauxController', function($scope) {
-      /* Here is your main controller */
+
+      $scope.question1 = false;
+      $scope.question2 = true;
+      $scope.question3 = true;
+      $scope.question4 = true;
+      $scope.question5 = true;
     });
