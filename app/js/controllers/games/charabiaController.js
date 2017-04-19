d265efa22a1d7a37886d2e21d4f7578ed05ca2a5
madasly <madasly@gmail.com>
2017-04-19 11:24:10 +0200

    page charabia terminée

--- app/js/controllers/games/charabiaController.js
+++ app/js/controllers/games/charabiaController.js
@@ -1,4 +1,17 @@
+var REPONSE = "C'EST TRÈS DANGEREUX DE TIRER SUR LE FIL D'UN APPAREIL ÉLECTRIQUE POUR LE DÉBRANCHER! IL FAUT TOUJOURS LE PRENDRE PAR LA FICHE POUR LE DÉCONNECTER.";
 angular.module('app')
-    .controller('CharabiaController', function($scope) {
-      /* Here is your main controller */
+      .controller('CharabiaController', function($scope) {
+      
+      $scope.newText = '';
+      $scope.compare = function () {
+        console.log('content', $scope.newText.content);
+        if ($scope.newText.toUpperCase()==REPONSE) {
+
+          console.log('Bravo!');
+        } else {
+
+        console.log('Il ne faut pas oublier de supprimer tous les z!');
+      }
+      };
     });
+//
