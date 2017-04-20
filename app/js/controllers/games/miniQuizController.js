f485e5899338eca850ab607237339307d53016e9
Cleclemi28 <c.trofleau@gmail.com>
2017-04-20 23:44:17 +0200

    le jeux miniquizz est fonctionel

--- app/js/controllers/games/miniQuizController.js
+++ app/js/controllers/games/miniQuizController.js
@@ -1,4 +1,9 @@
 angular.module('app')
     .controller('miniQuizController', function($scope) {
-      /* Here is your main controller */
+      $scope.vrai = function vrai(){
+        console.log("bravo");
+      };
+      $scope.faux = function faux(){
+        console.log("mauvaise reponse");
+      };
     });
